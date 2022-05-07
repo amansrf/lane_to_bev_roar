d40491f1612090e4cb216d5b92fdfbc905bd2478
amansrf <amansaraf99@gmail.com>
2022-05-07 15:04:11 -0700

    WIP Changes in many files
    Will be explained in future commits

--- lane_to_bev_roar/lane_detect_mask.py
+++ lane_to_bev_roar/lane_detect_mask.py
@@ -6,7 +6,13 @@ from cv_bridge import CvBridge
 import numpy as np
 from sensor_msgs.msg import Image
 
+import time
 
+color_mask_minima = {
+    "red_l": np.array([  0, 100,  200]),
+    "red_u": np.array([160, 100,  200]),
+    "white": np.array([   0,  0, 210]),
+}
 class MaskPublisher(Node):
 
     def __init__(self):
@@ -18,7 +24,7 @@ class MaskPublisher(Node):
 
     def image_callback(self, msg):
 
-        # Converting ROS image message to RGB
+        # Converting ROS image message to CV2 BGR8
         image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
 
         # Converting BGR to HSV
@@ -27,34 +33,92 @@ class MaskPublisher(Node):
         '''Since cv2 has two ranges for red in the HSV Color Space,
             we define two different masks and combine them.'''
         
+        # --------------------------------- Red Mask --------------------------------- #
         # Creating mask for the lower end of HSV red
-        lower_hue_minima = np.array([  0, 100,  200])
-        lower_hue_maxima = np.array([ 10, 255, 255])
-        lower_mask = cv2.inRange(converted_image, lower_hue_minima, lower_hue_maxima)
+        lower_red_minima = np.array([  0, 100,  200])
+        lower_red_maxima = np.array([ 10, 255, 255])
+        lower_mask = cv2.inRange(converted_image, lower_red_minima, lower_red_maxima)
 
         # Creating mask for the lower end of HSV red
-        upper_hue_minima = np.array([160, 100,  200])
-        upper_hue_maxima = np.array([179, 255, 255])
-        upper_mask = cv2.inRange(converted_image, upper_hue_minima, upper_hue_maxima)
+        upper_red_minima = np.array([160, 100,  200])
+        upper_red_maxima = np.array([179, 255, 255])
+        upper_mask = cv2.inRange(converted_image, upper_red_minima, upper_red_maxima)
 
-        # Combining the masks
-        mask = cv2.bitwise_or(lower_mask, upper_mask)
-        cv2.imshow("mask", mask)
-        cv2.waitKey(1)
+        # Combining the red masks
+        red_mask = cv2.bitwise_or(lower_mask, upper_mask)
+        # cv2.imshow("Red Mask", red_mask)
+        # cv2.waitKey(1)
 
-        # # Remove noisy detections from mask
-        # denoised_mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, self.denoising_kernel)
-        # cv2.imshow("denoised_mask", denoised_mask)
+        # Use a horizontal kernel to accentuate horizontal waypoint lines
+        horizontal_kernel = np.array(
+            [
+                [0, 0, 0],
+                [1, 1, 1],
+                [0, 0, 0]
+            ],
+            dtype = np.uint8
+        )
+
+        vertical_kernel = np.array(
+            [
+                [0, 1, 0],
+                [0, 1, 0],
+                [0, 1, 0]
+            ],
+            dtype = np.uint8
+        )
+
+        # First Erode and then Dilate using the horizontal_kernel to remove noise
+        red_mask = cv2.morphologyEx(red_mask, cv2.MORPH_OPEN, horizontal_kernel)
+
+        # Now dilate and then erode the image to join any internal noise or line breakages
+        red_mask = cv2.morphologyEx(red_mask, cv2.MORPH_CLOSE, np.ones((5,5)))
+        
+
+        # -------------------------------- White Mask -------------------------------- #
+        # Creating mask for HSV white
+        white_minima = np.array([   0,  0, 210])
+        white_maxima = np.array([ 255, 25, 255])
+
+        # Crop out top third of image for mask
+        converted_image[:converted_image.shape[0]//3,:] = 0
+
+        # Finish white mask
+        white_mask = cv2.inRange(converted_image, white_minima, white_maxima)
+        # cv2.imshow("white_mask", white_mask)
+        # cv2.waitKey(1)
+
+        # Use a horizontal kernel to accentuate horizontal waypoint lines
+        # horizontal_kernel = np.array(
+        #     [
+        #         [0, 0, 0],
+        #         [1, 1, 1],
+        #         [0, 0, 0]
+        #     ],
+        #     dtype = np.uint8
+        # )
+
+        # First Erode and then Dilate using the horizontal_kernel to remove noise
+        # white_mask = cv2.morphologyEx(white_mask, cv2.MORPH_OPEN, vertical_kernel)
+        # cv2.imshow("white_mask_without_noise", white_mask)
+
+        # Now dilate and then erode the image to join any internal noise or line breakages
+        # white_mask = cv2.morphologyEx(white_mask, cv2.MORPH_CLOSE, np.ones((5,5)))
+        # cv2.imshow("white_mask_closed", white_mask)
         # cv2.waitKey(1)
 
         # Halving the mask value from 255 because pointcloud overflows in some unexplainable way otherwise
-        mask = mask/2
+        red_mask = red_mask/2
+        white_mask = white_mask/2
 
-        # Stack the black and white image thrice like in RGB format.
+        # Stack the masks in bgr format.
         mask_stack = np.array(
-            np.stack((mask, mask, mask), axis=-1),
+            np.stack((white_mask, np.zeros(red_mask.shape), red_mask), axis=-1),
             dtype=np.uint8,
         )
+        cv2.imshow("combined_mask", (mask_stack*2)%255)
+        cv2.waitKey(1)
+
 
         # Convert to ros_msg
         masked_image = self.bridge.cv2_to_imgmsg(mask_stack, encoding='bgr8')
@@ -62,6 +126,8 @@ class MaskPublisher(Node):
 
         # Publish Masked Image
         self.masked_img_pub.publish(masked_image)
+
+    def color_mask(self, color):
 
 
 def main(args=None):
--- lane_to_bev_roar/pcl_to_ogm.py
+++ lane_to_bev_roar/pcl_to_ogm.py
@@ -6,6 +6,7 @@ import cv2
 from cv_bridge import CvBridge
 import numpy as np
 import math
+import time
 
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs.msg import Image
@@ -34,49 +35,131 @@ class BEVPublisher(Node):
             depth=1,
         )
         self.pointcloud_sub = self.create_subscription(PointCloud2, '/pointcloud', self.pcl_callback, qos_profile)
-        print("haha1")
+
         self.bev_img_pub = self.create_publisher(Image, 'bev_image', 1)
         self.bridge = CvBridge()
-        self.bev_shape = (256,256)
+        self.bev_shape = (512,512,3)
         self.denoising_kernel = np.ones((25,25), dtype=np.uint8)
         self.bev_image = Image()
 
     def pcl_callback(self, msg):
+
+        # Initialize empty bev_image
         bev_image = np.zeros(self.bev_shape)
+        # self.get_logger().info(f"BEV Shape is:{bev_image.shape}")
 
+        # Convert pcl2 message to numpy array
         pcl_arr = pointcloud2_to_array(msg, squeeze=False)
+        # self.get_logger().info(f"after pcl2_to_arr:{pcl_arr.shape}")
+
+        # Split the 32 bit rgb field into individual r,g,b values
         pcl_arr = split_rgb_field(pcl_arr)
+        # self.get_logger().info(f"after split_rgb_field:{pcl_arr.shape}")
+
+        # Convert pcl_arr to 6xN array
         pcl_arr = get_xyzrgb_points(pcl_arr)
+        # self.get_logger().info(f"after get_xyzrgb_points:{pcl_arr.shape}")
 
-        lane_coods = pcl_arr[np.argwhere(pcl_arr[...,3]>100)[:,0]]
-        lane_coods_xz = lane_coods[:,(0,2)]
-        
-        self.get_logger().info("# ------------------------------------ END ----------------------------------- #")
-        # self.get_logger().info(f"{(lane_coods).shape}")
-        # self.get_logger().info(f"{(lane_coods_xz).shape}")
+        # Filter locations of masked points only
+        # lane_coods = pcl_arr[np.argwhere(pcl_arr[:,3]>100)[:,0]]
+        lane_coods = pcl_arr[np.where( (pcl_arr[:,3]>100) | (pcl_arr[:,5]>100))[0]]
+        # self.get_logger().info(f"Lane_Coods after mask filter:{lane_coods.shape}")
 
-        # lane_coods_xz = np.argwhere((lane_coods_xz[...,1]<MAX_Z))
+        # self.get_logger().info(f"Lane_Coods after x filter:{np.where( (lane_coods[:,0] > MIN_X) & (lane_coods[:,0] < MAX_X))[0].shape}")
+        # Filter points by x range
+        lane_coods = lane_coods[
+            np.where(
+                (lane_coods[:,0] > MIN_X) & (lane_coods[:,0] < MAX_X)
+            )[0],
+            :
+        ]
+        # self.get_logger().info(f"Lane_Coods after x filter:{lane_coods.shape}")
 
-        # self.get_logger().info(f"{(pcl_arr).shape}")
-        # self.get_logger().info(f"{(lane_coods_xz).shape}")
+        # Filter points by z range
+        lane_coods = lane_coods[
+            np.where(
+                (lane_coods[:,2] > MIN_Z) & (lane_coods[:,2] < MAX_Z)
+            )[0],
+            :
+        ]
+        # self.get_logger().info(f"Lane_Coods after z filter:{lane_coods.shape}")
 
-        lane_coods_uv = np.zeros(lane_coods_xz.shape)
-        lane_coods_uv[:,0] = np.floor( self.bev_shape[1] * (lane_coods_xz[:,0] - MIN_X)/(MAX_X-MIN_X + 1e-10))
-        lane_coods_uv[:,1] = np.floor( self.bev_shape[0] * (lane_coods_xz[:,1] - MIN_Z)/(MAX_Z-MIN_Z + 1e-10))
 
-        self.get_logger().info(f"{lane_coods_uv[:5]}")
-        self.get_logger().info(f"{lane_coods_xz[:5]}")
+        # Extract x and z coods of filtered points
+        # lane_coods_x = lane_coods[:,0]
+        lane_coods_x = lane_coods[np.where(lane_coods[:,3]>100)[0], 0]
+        # self.get_logger().info(f"Max x:{np.max(lane_coods_x)}, Min x:{np.min(lane_coods_x)}")
+
+        # lane_coods_z = lane_coods[:,2]
+        lane_coods_z = lane_coods[np.where(lane_coods[:,3]>100)[0], 2]
+        # self.get_logger().info(f"Max z:{np.max(lane_coods_z)}, Min z:{np.min(lane_coods_z)}")
+
+        lane_coods_uv = np.zeros( (lane_coods_x.shape[0], 2) )
+
+        lane_coods_uv[:,0] = np.floor( self.bev_shape[1] * (lane_coods_x[:] - MIN_X)/(MAX_X-MIN_X + 1e-10))
+        # self.get_logger().info(f"Max u:{np.max(lane_coods_uv[:,0])}, Min u:{np.min(lane_coods_uv[:,0])}")
+
+        lane_coods_uv[:,1] = np.floor( self.bev_shape[0] * (lane_coods_z[:] - MIN_Z)/(MAX_Z-MIN_Z + 1e-10))
+        # self.get_logger().info(f"Max v:{np.max(lane_coods_uv[:,1])}, Min v:{np.min(lane_coods_uv[:,1])}")
+
+
         lane_coods_uv = np.array(lane_coods_uv, dtype=np.uint)
-        bev_image[ self.bev_shape[0]-lane_coods_uv[:,1]-1 , lane_coods_uv[:,0] ] = 1
+        # self.get_logger().info(f"{( (self.bev_shape[0]-lane_coods_uv[:,1]-1)%255 , lane_coods_uv[:,0]%255 )}")
+        bev_image[ (self.bev_shape[0]-lane_coods_uv[:,1]-1) , lane_coods_uv[:,0], 2] = 1
+
+        # Extract x and z coods of filtered points
+        # lane_coods_x = lane_coods[:,0]
+        lane_coods_x = lane_coods[np.where(lane_coods[:,5]>100)[0], 0]
+        # self.get_logger().info(f"Max x:{np.max(lane_coods_x)}, Min x:{np.min(lane_coods_x)}")
+
+        # lane_coods_z = lane_coods[:,2]
+        lane_coods_z = lane_coods[np.where(lane_coods[:,5]>100)[0], 2]
+        # self.get_logger().info(f"Max z:{np.max(lane_coods_z)}, Min z:{np.min(lane_coods_z)}")
+
+        lane_coods_uv = np.zeros( (lane_coods_x.shape[0], 2) )
+
+        lane_coods_uv[:,0] = np.floor( self.bev_shape[1] * (lane_coods_x[:] - MIN_X)/(MAX_X-MIN_X + 1e-10))
+        # self.get_logger().info(f"Max u:{np.max(lane_coods_uv[:,0])}, Min u:{np.min(lane_coods_uv[:,0])}")
+
+        lane_coods_uv[:,1] = np.floor( self.bev_shape[0] * (lane_coods_z[:] - MIN_Z)/(MAX_Z-MIN_Z + 1e-10))
+        # self.get_logger().info(f"Max v:{np.max(lane_coods_uv[:,1])}, Min v:{np.min(lane_coods_uv[:,1])}")
+
+
+        lane_coods_uv = np.array(lane_coods_uv, dtype=np.uint)
+        # self.get_logger().info(f"{( (self.bev_shape[0]-lane_coods_uv[:,1]-1)%255 , lane_coods_uv[:,0]%255 )}")
+        bev_image[ (self.bev_shape[0]-lane_coods_uv[:,1]-1) , lane_coods_uv[:,0], 0] = 1
 
 
         cv2.imshow("bev", bev_image)
         cv2.waitKey(1)
 
         # Remove noisy detections from mask
-        denoised_image = cv2.morphologyEx(bev_image, cv2.MORPH_CLOSE, self.denoising_kernel)
 
-        cv2.imshow("bev_noiseless", denoised_image)
+        # vertical_kernel = np.array(
+        #     [
+        #         [0, 0, 1, 1, 1, 0, 0],
+        #         [0, 0, 1, 1, 1, 0, 0],
+        #         [0, 0, 1, 1, 1, 0, 0],
+        #         [0, 0, 1, 1, 1, 0, 0],
+        #         [0, 0, 1, 1, 1, 0, 0],
+        #         [0, 0, 1, 1, 1, 0, 0],
+        #         [0, 0, 1, 1, 1, 0, 0],
+        #     ],
+        #     dtype = np.uint8
+        # )
+        vertical_kernel = np.array(
+            [
+                [0, 1, 0],
+                [0, 1, 0],
+                [0, 1, 0],
+            ],
+            dtype = np.uint8
+        )
+
+        # denoised_image = cv2.morphologyEx(bev_image, cv2.MORPH_CLOSE, vertical_kernel)
+        denoised_image = cv2.dilate(bev_image, vertical_kernel, iterations=8)
+
+        cv2.imshow("~/bev_noiseless.jpg", denoised_image)
         cv2.waitKey(1)
 
         denoised_image = np.array(
@@ -103,13 +186,13 @@ def get_xyzrgb_points(cloud_array, remove_nans=True, dtype=np.float):
         cloud_array = cloud_array[mask]
 
     # pull out x, y, and z values
-    points = np.zeros(cloud_array.shape + (4,), dtype=dtype)
+    points = np.zeros(cloud_array.shape + (6,), dtype=dtype)
     points[...,0] = cloud_array['x']
     points[...,1] = cloud_array['y']
     points[...,2] = cloud_array['z']
     points[...,3] = cloud_array['r']
-    # points[...,4] = cloud_array['g']
-    # points[...,5] = cloud_array['b']
+    points[...,4] = cloud_array['g']
+    points[...,5] = cloud_array['b']
     
     return points
 
